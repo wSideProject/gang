f06b6ee5b471788d71de499d0bb418cab25a7c45
rachel <dev.rachelxx@gmail.com>
2023-03-01 22:51:27 +0900

    git initialize

---
+++ src/main/java/com/wside/license/com/wside/license/controller/LicenseController.java
@@ -0,0 +1,10 @@
+package com.wside.license.com.wside.license.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("value=api/v1/organization/{organizationId}/license")
+public class LicenseController {
+
+}
